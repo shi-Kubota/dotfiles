4576646360b5f9584d5e5db3af07701fcc2aefdf
Shinya Kubota <shinya.kubota.051@gmail.com>
2015-03-04 22:38:43 +0900

    dircolors変更

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/dotfiles/dircolors-solarized/dircolors.256dark ~/.dircolors
+ln -s ~/dotfiles/dircolors-zenburn ~/.dircolors
 
